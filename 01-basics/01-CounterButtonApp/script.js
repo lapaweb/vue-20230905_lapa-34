6204ec620890351d05ad8747992e244fa6f2daa8
Ekaterina Kirichenko <lapa-34@ya.ru>
2023-09-11 13:56:53 +0300

    01-basics/4-tasks

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,24 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+// import { count } from 'console';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const app = createApp({
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  
+});
+
+// console.log(app.mount('#app'))
+app.mount('#app')
+
+
+
