520a91478672e352cdcbf9ed0a00b65648854504
Ekaterina Kirichenko <lapa-34@ya.ru>
2023-11-22 18:40:43 +0300

    add 07/01-02 tasks + fix 06/05

--- 07-forms/02-MeetupForm/components/UiImageUploader.vue
+++ 07-forms/02-MeetupForm/components/UiImageUploader.vue
@@ -1,68 +1,113 @@
-<!-- STUB: ЭТО ЗАГЛУШКА ДЛЯ РУЧНОГО ТЕСТИРОВАНИЯ -->
-<!-- ВЫ МОЖЕТЕ ИСПОЛЬЗОВАТЬ ПОЛНУЮ ВЕРСИЮ КОМПОНЕНТА, ЕСЛИ УЖЕ РЕАЛИЗОВАЛИ ЕГО -->
-
 <template>
   <div class="image-uploader">
-    <label class="image-uploader__preview" :style="src && `--bg-url: url('${src}')`" @click.stop.prevent="handleClick">
-      <span class="image-uploader__text">{{ src ? 'Удалить' : 'Загрузить изображение' }}</span>
-      <input
-        ref="input"
-        type="file"
-        accept="image/*"
-        class="image-uploader__input"
-        v-bind="$attrs"
-        @change="mockFileSelect"
-      />
-    </label>
-  </div>
+    <label 
+      class="image-uploader__preview"  
+      :class="{'image-uploader__preview-loading': state==='load'}" 
+      :style="previewNew" 
+      >
+       <span class="image-uploader__text">{{ inputText }}</span>
+        <input 
+         ref="fileInput"
+         type="file"
+         v-bind="$attrs"
+         accept="image/*" 
+         class="image-uploader__input"
+         @change=attachImage($event)
+         @click=deleteImage()
+         />
+      </label>
+   </div>
 </template>
 
 <script>
+
 export default {
   name: 'UiImageUploader',
+
   inheritAttrs: false,
 
+  emits: ['remove', 'select', 'error', 'upload'],
+
   props: {
-    uploader: {
-      type: Function,
-    },
-
-    preview: {
-      type: String,
-    },
+    preview: String,
+    uploader: Function,
   },
-
-  emits: ['upload', 'select', 'error', 'remove'],
 
   data() {
     return {
-      src: this.preview,
-    };
+      previewNew: '',
+      value: '',
+      state: 'empty',
+    }
   },
 
-  methods: {
-    mockFileSelect() {
-      this.src = 'https://course-vue.javascript.ru/api/images/1';
-      const file = new File(['abc'], 'abc.jpeg', {
-        type: 'image/jpeg',
-      });
-      this.$emit('select', this.$refs.input.files[0] || file);
-    },
-
-    mockRemoveFile() {
-      this.src = null;
-      this.$refs.input.value = '';
-      this.$emit('remove');
-    },
-
-    handleClick() {
-      if (this.src && this.src !== this.preview) {
-        this.mockRemoveFile();
-      } else {
-        this.mockFileSelect();
+  computed: {
+    inputText() {
+      if (this.state == 'delete') {
+        return 'Загрузить изображение'
       }
+      if (this.preview || this.state == 'full') {
+        return 'Удалить изображение'
+      }
+      if (this.state == 'load') {
+        return 'Загрузка...'
+      }
+      return 'Загрузить изображение';
     },
   },
+  
+  created() {
+    this.getDefoultPreview();
+  },
+  
+  methods: {
+    downloadUploadFile() {
+      this.state = 'load'
+    },
+    getDefoultPreview() {
+      this.previewNew = this.preview ? `--bg-url: url('${this.preview}')` : "--bg-url: var(--default-cover)"
+    },
+    handleUploaderSucces(result) {
+      this.$emit('upload', result);
+      this.previewNew = `--bg-url: url('${result.image}')`
+      this.state = 'full';
+    },
+    handleUploaderError(error) {
+      this.$emit('error', error);
+      this.$refs['fileInput'].value = null;
+      this.state = 'empty';
+    },
+    attachImage($event) {
+      const files = $event.target.files;
+      if (files.length) {
+          this.$emit('select', files[0]);    
+          if (this.uploader) {
+              this.downloadUploadFile();
+              this.uploader(files[0])
+                .then((result) => this.handleUploaderSucces(result))
+                .catch((error) => this.handleUploaderError(error))
+            }
+          else if(this.uploader == null) {
+              this.previewNew = `--bg-url: url('${window.URL.createObjectURL(files[0])}')`;
+              this.state = 'full';
+            }
+        }
+      },
+      deleteImage() {
+        if (this.$refs['fileInput'].value === null) {
+          return;
+        }
+        if (this.state == 'load') {
+          return;
+        }
+        this.previewNew =  "--bg-url: var(--default-cover)";
+        this.$refs['fileInput'].value = null;
+        this.$emit('remove');
+        this.state = 'delete';
+    },
+   }
+
+  
 };
 </script>
 
@@ -76,7 +121,9 @@ export default {
 }
 
 .image-uploader__preview {
-  --bg-url: var(--default-cover);
+  /* --bg-url: var(--default-cover); */
+  /* --bg-url: v-bind(imageUrl); */
+  /* --bg-url: "https://course-vue.javascript.ru/api/images/1" */
   background-size: cover;
   background-position: center;
   background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--bg-url);
